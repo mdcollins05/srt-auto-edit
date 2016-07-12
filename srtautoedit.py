96865a68fb4bb1ee33d5440ca0b22d95d725ad75
MDCollins <matt@m-collins.com>
2016-07-11 18:58:02 -0800

    Further work on everything. Adding a method to validate regex before attempting to process any srt files

--- srtautoedit.py
+++ srtautoedit.py
@@ -16,6 +16,7 @@ def main():
 
   argsparser = argparse.ArgumentParser(description="Automatically apply a set of rules to srt files")
   argsparser.add_argument("srt", help="SRT file or directory to operate on")
+  argsparser.add_argument("--dry-run", "-d", help="Dry run. This will not make any changes and instead will tell you what it would do")
 
   args = argsparser.parse_args()
 
@@ -25,12 +26,22 @@ def main():
     for root, dirs, files in os.walk(args.srt):
       for file in files:
         if file.endswith(".srt"):
-          parse_srt(settingsYaml, os.path.join(root, file))
+          parse_srt(settingsYaml, os.path.join(root, file), args.dry_run)
   else:
     print("'{0}' doesn't exist".format(args.srt))
 
-def parse_srt(settings, file):
+def parse_srt(settings, file,):
   print("Parsing '{0}'...".format(file))
+  subs = pysrt.open(file)
+  #do things
+  subs.save(file)
+
+def validate_regex(settingsYaml):
+  #Do some validation of any regex rules passed
+  #try:
+  #  re.compile(regex)
+  #except re.error:
+  #  #it's wrong
   pass
 
 if __name__ == "__main__":
