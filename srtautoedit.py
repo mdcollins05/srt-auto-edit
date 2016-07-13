e9aa788b8cab7f4792548fb37ee7f6c0f8c90bbd
MDCollins <matt@m-collins.com>
2016-07-12 21:21:11 -0800

    Moar work done. Almost ready for some testing

--- srtautoedit.py
+++ srtautoedit.py
@@ -30,19 +30,48 @@ def main():
   else:
     print("'{0}' doesn't exist".format(args.srt))
 
-def parse_srt(settings, file,):
+def parse_srt(settings, file, dry_run):
   print("Parsing '{0}'...".format(file))
-  subs = pysrt.open(file)
-  #do things
-  subs.save(file)
 
-def validate_regex(settingsYaml):
-  #Do some validation of any regex rules passed
-  #try:
-  #  re.compile(regex)
-  #except re.error:
-  #  #it's wrong
-  pass
+  try:
+    subtitles = pysrt.open(file)
+  except:
+    print("Couldn't open file {0}".format(file)
+    return
+
+  for subtitle in subtitles:
+    for rule in settings:
+      if rule['type'] == 'regex':
+        if rule['action'] == 'replace':
+          pass # re.sub
+        elif rule['action'] == 'delete':
+          pass # re.match
+        else:
+          print("Unknown action: {0}".format(rule['action']))
+      elif rule['type'] == 'string':
+        if rule['action'] == 'replace':
+          pass # subtitle.replace
+        elif rule['action'] == 'delete':
+          pass # subtitle.find
+        else:
+          print("Unknown action: {0}".format(rule['action']))
+      else:
+        print("Unknown type: {0}".format(rule['type']))
+
+  if not dry_run:
+    subtitles.save(file)
+
+def validate_regex(settings):
+  for rule in settings:
+    if type == "regex":
+      rule['regex'] = compile_regex(rule['pattern'])
+  return settings
+
+def compile_regex(regex):
+  try:
+   return re.compile(regex, re.IGNORECASE)
+  except re.error:
+    print("Error with regex: {0}".format(regex))
 
 if __name__ == "__main__":
   sys.exit(main())
