45246b1f5e9b9d3cc954e44c5e85272b4a2a0d92
MDCollins <matt@m-collins.com>
2016-09-01 19:45:13 -0800

    Added the ability to specify the path to the configuration file instead of it being hardcoded

--- srtautoedit.py
+++ srtautoedit.py
@@ -11,17 +11,21 @@ import sys
 def main():
   settingsYaml = {}
 
-  if os.path.isfile("settings.yaml"):
-    settingsFile = open("settings.yaml")
-    settingsYaml = yaml.safe_load(settingsFile)
-    settingsFile.close()
-
   argsparser = argparse.ArgumentParser(description="Automatically apply a set of rules to srt files")
   argsparser.add_argument("srt", help="SRT file or directory to operate on")
+  argsparser.add_argument("--config", "-c", default="settings.yaml", help="Specify an alternative location to find the settings configuration file")
   argsparser.add_argument("--dry-run", "-d", action="store_true", help="Dry run. This will not make any changes and instead will tell you what it would do")
   argsparser.add_argument("--verbose", "-v", action="store_true", help="Verbose output. Lines that have changed will be printed on screen")
 
   args = argsparser.parse_args()
+
+  if os.path.isfile(args.config):
+    settingsFile = open(args.config)
+    settingsYaml = yaml.safe_load(settingsFile)
+    settingsFile.close()
+  else:
+    print("Couldn't open configuration file")
+    return False
 
   if os.path.isfile(args.srt):
     parse_srt(settingsYaml, args.srt, args.dry_run, args.verbose)
