4a727a18c64291df15bcd5634946bdd0b2f6dcf7
MDCollins <matt@m-collins.com>
2018-01-19 23:56:10 -0800

    Add Radarr and Sonarr post processing scripts

---
+++ sonarr-postprocess.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# Run srt-auto-edit against any subtitle files included with video file for Sonarr
+
+DESTINATION=$sonarr_series_path
+SCRIPT_PATH="/home/matt/srt-auto-edit" #EDIT ME!
+CONFIG_PATH="/home/matt/srt-auto-edit/settings.yaml" #EDIT ME!
+
+${SCRIPT_PATH}/srtautoedit.py -c "$CONFIG_PATH" "$DESTINATION"
