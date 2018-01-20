6a336c31647240abde8b3b9b5a40f8d0bfe607db
MDCollins <matt@m-collins.com>
2018-01-19 23:05:57 -0800

    Use quiet summary by default in NZBGet PP script

--- nzbget-postprocess.sh
+++ nzbget-postprocess.sh
@@ -11,13 +11,16 @@
 # Path to srt-auto-edit settings.yaml file
 #SETTINGSYAML_PATH=
 
+# Script arguments
+#SCRIPT_ARGS="-s -q"
+
 ### NZBGET POST-PROCESSING SCRIPT                                          ###
 ##############################################################################
 
 #Clean the mkv (will make a clean.<filename>
 
 echo "Running srt-auto-edit on files..."
-python srtautoedit.py -c "$NZBPO_SETTINGSYAML_PATH" "$NZBPP_DIRECTORY"
+python srtautoedit.py -c "$NZBPO_SETTINGSYAML_PATH" $NZBPO_SCRIPT_ARGS "$NZBPP_DIRECTORY"
 
 # Exit good no matter what
 exit 93
